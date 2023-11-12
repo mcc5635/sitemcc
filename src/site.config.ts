17f9c63366b1ca8cc53c4898bf5171af5e9a3402
Michael Chase <85896593+mcc5635@users.noreply.github.com>
2023-11-12 14:42:28 -0500

    Update site.config.ts

--- src/site.config.ts
+++ src/site.config.ts
@@ -2,11 +2,11 @@ import type { SiteConfig } from "@/types";
 
 export const siteConfig: SiteConfig = {
 	// Used as both a meta property (src/components/BaseHead.astro L:31 + L:49) & the generated satori png (src/pages/og-image/[slug].png.ts)
-	author: "Chris Williams",
+	author: "Michael Chase",
 	// Meta property used to construct the meta title property, found in src/components/BaseHead.astro L:11
-	title: "Astro Theme Cactus",
+	title: "Michael Chase",
 	// Meta property used as the default description meta property
-	description: "An opinionated starter theme for Astro",
+	description: "personal website",
 	// HTML lang property, found in src/layouts/Base.astro L:18
 	lang: "en-GB",
 	// Meta property, found in src/components/BaseHead.astro L:42
@@ -23,7 +23,7 @@ export const siteConfig: SiteConfig = {
 	// Include view-transitions: https://docs.astro.build/en/guides/view-transitions/
 	includeViewTransitions: false,
 	webmentions: {
-		link: "https://webmention.io/astro-cactus.chriswilliams.dev/webmention",
+		link: "https://twitter.com/mikechase_",
 	},
 };
 
@@ -32,13 +32,5 @@ export const menuLinks: Array<{ title: string; path: string }> = [
 	{
 		title: "Home",
 		path: "/",
-	},
-	{
-		title: "About",
-		path: "/about/",
-	},
-	{
-		title: "Blog",
-		path: "/posts/",
 	},
 ];
